f8d6282b6bb21e7a03da6faf484b8513c9d03f53
Sharma-Ayush <ayushsharma812@gmail.com>
2025-01-03 22:30:07 +0530

    Distribution package config setup in setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .' # String for running setup.py within dependency file and generating the ditribution package
+
+def get_dependencies(dependency_file:str) -> List[str]:
+    '''Read the dependencies from given file path and return as a list of strings'''
+    
+    dependencies = []
+    
+    with open(dependency_file, 'r') as file:
+        for line in file:
+            stripped_line = line.strip()
+            if stripped_line != HYPEN_E_DOT:
+                dependencies.append(stripped_line)
+
+    return dependencies
+
+setup(
+    name = 'ieee_cis_fraud_detection',
+    version = '0.0.1',
+    author = 'Ayush Sharma',
+    author_email = 'ayushsharma812@gmail.com',
+    packages = find_packages(),
+    install_requires = get_dependencies('requirements.txt')
+    )
