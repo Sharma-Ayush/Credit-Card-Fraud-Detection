fc35565672353c21837f945394a6e5f34a5d3b48
Sharma-Ayush <ayushsharma812@gmail.com>
2025-01-16 00:48:12 +0530

    Change to relative file path for log outputs

--- src/logger.py
+++ src/logger.py
@@ -3,11 +3,11 @@ import logging.config
 import os
 import json
 
-# make log directory if it does not exist
+# make log directory, if it does not exist
 os.makedirs('logs', exist_ok = True)
 
 cwd = os.getcwd()
-config_file_path = os.path.join('', 'src/logging_config.json')
+config_file_path = os.path.join(cwd, 'src/logging_config.json')
 
 # Load the JSON configuration
 with open(config_file_path, 'r') as config_file:
@@ -15,5 +15,8 @@ with open(config_file_path, 'r') as config_file:
 
 # Setting up the config
 logging.config.dictConfig(config_dict)
+
+if __name__ == "__main__":
+    logging.info("This is working fine?")
 
 
