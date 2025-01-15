16da95a1338d2703ad01955e0cd153e708707456
Sharma-Ayush <ayushsharma812@gmail.com>
2025-01-16 01:59:24 +0530

    Define custom class for printing of exception messages

--- src/exception.py
+++ src/exception.py
@@ -1,20 +1,13 @@
 import sys
 from src.logger import logging
 
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-     file_name,exc_tb.tb_lineno,str(error))
-
-    return error_message
-
-    
-
-class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
-        super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+class CustomError(Exception):
+    '''Custom Exception class for making the print of exception message more informative'''
+    def __init__(self, type_of_error, exception_object, traceback):
+        super().__init__(exception_object.args)
+        self.type_of_error = type_of_error
+        self.exception_object = exception_object
+        self.traceback = traceback
     
     def __str__(self):
-        return self.error_message
+        return f"{self.type_of_error.__name__}: {self.exception_object}"
