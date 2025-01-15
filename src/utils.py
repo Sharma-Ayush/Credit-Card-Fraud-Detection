f3db63f25b34db2cb8af661c48a26ea518192b8e
Sharma-Ayush <ayushsharma812@gmail.com>
2025-01-16 01:58:31 +0530

    Add class description

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import os
 
 # Custom class so that we can print realtive path with respect to app.py
 class CustomFormatter(logging.Formatter):
+    '''Custom Formatter which replaces first occurence of full path ane with relative pathname of app.py'''
     def format(self, record):
         full_path = record.pathname
         relative_path = os.path.relpath(full_path)
