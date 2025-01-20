ffd7fc1bc4e59b32822f1432871e36ba8bc7f64a
Sharma-Ayush <ayushsharma812@gmail.com>
2025-01-20 13:43:01 +0530

    Add ModelEvaluator class

--- src/components/model_evaluator.py
+++ src/components/model_evaluator.py
@@ -1,1 +1,31 @@
-       
+from src.logger import logging
+
+from sklearn.preprocessing import StandardScaler, FunctionTransformer
+from sklearn.compose import ColumnTransformer
+from sklearn.decomposition import PCA
+from imblearn.pipeline import Pipeline
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBClassifier
+
+from sklearn.metrics import auc, precision_recall_curve
+
+class ModelEvaluator:
+    '''
+    A class for evaluating our model on given data.
+    '''
+    def evaluate(self, pre_processor, model, X, Y, scorer_func, name_for_data):
+        '''
+        Method Description: Model evaluation method.
+        Takes in the pre-processor, model, X, Y, scorer function, name or title of the data set and returns evaluation score.
+        '''
+        logging.info(f'Initiating model evaluation for {name_for_data} data set...')
+
+        X_transformed = pre_processor.transform(X)
+        Y_pred = model.predict_proba(X_transformed)
+
+        score = scorer_func(Y, Y_pred[:, 1])
+
+        logging.info(f'Successfully completed model evaluation for {name_for_data} data set with score  = {score}!!!')
+        return score 
