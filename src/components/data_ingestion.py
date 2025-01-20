451eeba404a44c0f4e632fa5c7b8d10f4e9dc94a
Sharma-Ayush <ayushsharma812@gmail.com>
2025-01-20 13:39:51 +0530

    Add DataIngestionConfig and DataIngestion classes

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,6 +1,4 @@
 import os
-import sys
-from src.exception import CustomError
 from src.logger import logging
 import pandas as pd
 
@@ -9,57 +7,54 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
-    '''A data class for storing paths related to data files storage'''
+    '''
+    A data class for storing paths related to data files storage
+    '''
     raw_data_path = os.path.join("artifacts", "data.csv")
     train_data_path = os.path.join("artifacts", "train.csv")
     test_data_path = os.path.join("artifacts", "test.csv")
 
 class DataIngestion:
-    '''To import data, split it into test-train data, and save the files in pre-defined paths'''
-
+    '''
+    A class for ingesting data to be used in training pipeline
+    '''
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
-
-    def load_data(self, path):
-        return pd.read_csv(path)
-    
-    def split_data(self, df, target_class, test_size, random_state):
-        return train_test_split(df.drop(target_class, axis = 1),
-                                df[target_class],
-                                stratify = df[target_class],
-                                test_size = test_size,
-                                random_state = random_state)
     
     def save_data(self, df, path):
+        '''
+        Save dataframe as a csv file in given path without index but with header included
+        '''
         df.to_csv(path, 
                   index = False,
                   header = True)
 
-    def ingest_data(self):
-        logging.info("Started data ingestion...")
-        try:
-            # load data
-            df = self.load_data('notebook/Data/creditcard.csv')
+    def ingest_data(self, path, target_class, test_size, random_state):
+        '''
+        Method Description: Data ingestion method
+        Loads the data from given csv file path, splits it into train-test data, saves them into csv files.
+        Returns the splitted data as a tuple of format (X_train, X_test, Y_train, Y_test)
+        '''
+        logging.info('Initiating data ingestion...')
 
-            # create required directories, if not exist
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok = True)
+        # load data and drop duplicates
+        df = pd.read_csv(path)
+        df.drop_duplicates(inplace = True)
 
-            # split the dataset
-            X_train, X_test, Y_train, Y_test = self.split_data(df, 
-                                                               target_class = 'Class',
-                                                               test_size = 0.2,
-                                                               random_state = 42)
+        # create required directories, if not exist
+        os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok = True)
 
-            # save the data to files
-            self.save_data(df, self.ingestion_config.raw_data_path)
-            self.save_data(pd.concat((X_train, Y_train), axis = 1), self.ingestion_config.train_data_path)
-            self.save_data(pd.concat((X_test, Y_test), axis = 1), self.ingestion_config.test_data_path)
-        except:
-            error_obj = CustomError(*sys.exc_info())
-            logging.error(error_obj, exc_info = True)
-            raise error_obj
-        else:
-            logging.info("Successfully completed data ingestion!!!")
+        # split the dataset
+        X_train, X_test, Y_train, Y_test = train_test_split(df.drop(target_class, axis = 1),
+                                                            df[target_class],
+                                                            stratify = df[target_class],
+                                                            test_size = test_size,
+                                                            random_state = random_state)
 
-            return (self.ingestion_config.train_data_path,
-                    self.ingestion_config.test_data_path)
+        # save the data to files
+        self.save_data(df, self.ingestion_config.raw_data_path)
+        self.save_data(pd.concat((X_train, Y_train), axis = 1), self.ingestion_config.train_data_path)
+        self.save_data(pd.concat((X_test, Y_test), axis = 1), self.ingestion_config.test_data_path)
+
+        logging.info("Successfully completed data ingestion!!!")
+        return (X_train, X_test, Y_train, Y_test)
